937aa679c832dddfdac6c97684ed08ad58fe191e
Tanaka <tonkatu0741@gmail.com>
2025-04-13 22:25:52 +0900

    2025/04/13

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//https://web-camp.io/magazine/archives/91511/より
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
